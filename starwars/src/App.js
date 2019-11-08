c53296644904424aaac1602819ac4f6afbe4e7ec
Katrina Finney <katrinafinney@gmail.com>
2019-11-08 16:27:57 -0500

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import StarToast from "./components/StarToast"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,12 +9,39 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+     
+      .then(response => {
+        console.log(response.data);
+
+        setData(response.data.results);
+         
+      })
+      .catch(error => {
+        console.log("The data was not returned", error);
+      }); 
+  }, []);
+
+  
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+
+      <h1 className="Header" style={{ background: 'black' , color: 'white', textAlign: 'center'}}>I Conquered 'React Wars'</h1>
+
+     {data.map((starPeople, index) => {
+          return <StarToast key={index} name={starPeople.name} height={starPeople.height} birthday={starPeople.birth_year}
+          mass={starPeople.mass} gender={starPeople.gender}
+          />
+        })}
+      
     </div>
+    
   );
-}
+ 
+  }
 
 export default App;
